c2af693284802c5de7e4750ee0254491662b29ab
NelepinAndrey <andreqnelepin@gmail.com>
2019-01-03 17:03:08 +0200

    SignIn & SignUp

---
+++ src/sample/SignUpController.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private TextField signUpLastName;
+
+    @FXML
+    private TextField signUpName;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
